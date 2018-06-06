e659423ab4d60e05e5ad8e7b5c21f4fd5a7752be
shabbyjoon <ShabnamMokhtarani@Shabnams-MacBook-Pro.local>
2018-06-05 19:34:24 -0700

    implemented snippet name and content display

--- app.js
+++ app.js
@@ -1,34 +1,49 @@
 $(document).ready(function(){
-
-	let mySnippets = {
-		'snippetName' : snippetText
-	}
+  var nameFieldValue;
+  var textFieldValue;
+   
+   var mySnippets = {
+	  	'myNameTextData' : nameFieldValue,
+	    'myFormTextData' : textFieldValue
+	    }
 
   $('.setForm').on('click', function(){
   	let textFieldValue = $('.textField').val();
   	$('.debug').text(textFieldValue);
 
-  	localStorage.setItem('snippetText', textFieldValue);
+  	localStorage.setItem('myFormTextData', textFieldValue);
 
   });
 
    $('.setName').on('click', function(){
    	let nameFieldValue = $('.nameField').val();
+   	var mySnippets = {
+	 	'myNameTextData' : nameFieldValue,
+		'myFormTextData' : textFieldValue
+	 }
    	$('.debug').text(nameFieldValue);
 
+  console.log(nameFieldValue);
    	localStorage.setItem('myNameTextData', nameFieldValue);
 
   	
   });
 
   $('.getForm').on('click', function(){
-  	let retrieveSnippet = localStorage.getItem('snippetText');
-  	$('.debug').text(retrieveSnippet);
+  	let retrieveData = localStorage.getItem('myFormTextData');
+  	$('.debug').text(retrieveData);
   });
 
   $('.getName').on('click', function(){
-  	let retrieveName = localStorage.getItem('myNameTextData');
-  	$('.debug').text(retrieveName);
+  	let retrieveData = localStorage.getItem('myNameTextData');
+  	$('.debug').text(retrieveData);
+  });
+
+    $('.displayAll').on('click', function(){
+    let retrieveName =localStorage.getItem('myNameTextData');
+    let retrieveForm = localStorage.getItem('myFormTextData')
+    $('.debug').text(retrieveName);
+    $('.getAll').text(retrieveForm);
   });
   // $('.textField').on('keyup', function(){
   // 	let textFieldValue = $('.textField').val();
